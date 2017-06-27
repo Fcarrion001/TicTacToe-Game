6d7d1c21cae219fd078d9067c5041c0c9e69e57a
fcarrion001 <fpc1118@gmail.com>
2017-06-27 19:48:33 -0400

    Added game logic to index.js, altered html
    created game logic that inputs either an "x" or an "o"
    depending on who's turn it is. If the square already
    has a value nothing will happen in the square.
    I also removed the modal from the html for now and
    added an events.js file.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -11,4 +11,28 @@ $(() => {
 // const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
-require('./example')
+const events = require('./events.js')
+
+
+
+//create function to input "X" and "O" values for game
+// define variable to keep track of turn
+let turnNum = 0
+
+$((ticTacToe) => {
+  $('#board').on('click', function(event) {
+    event.preventDefault()
+    const textValue = $(event.target).text()
+    // prevent inputing a value on a square the has been used already
+      if (textValue === 'X' || textValue === 'O') {
+      console.log('pick a valid square')
+    } else if (turnNum % 2 !== 0) {
+        turnNum++
+        $(event.target).text('O')
+    } else {
+        turnNum++
+        $(event.target).text('X')
+
+      }
+  })
+})
