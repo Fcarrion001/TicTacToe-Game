955538f3000b9bd81f6e0ff0ef1f3bfe915b8b56
fcarrion001 <fpc1118@gmail.com>
2017-06-29 23:20:19 -0400

    cleaned code, added style, made message box functional
    message box now displays who won, if signing
    in and up and changing password was successful, and the number of games
    played by the registered user. I turned long pieces of code into funtions.
    I added a bit of css and a description of the game.  finally I completed
    the README.md file.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 PROJECT PLAN
+
 1. Sketch wireframes and write user stories
 2. Make a diagram
 3. Create a repo, and README file
@@ -14,3 +15,36 @@ PROJECT PLAN
 13. Write AJAX code.
 14. Add additional features
 15. Finish documentation.
+
+url for wirframe
+http://franklincarrion.imgur.com/all/?third_party=1
+
+user stories
+
+- 1. As a developer, I want to use AMD, so that I can write modular code.
+      2. As a developer, I want to make my code readable, so that it can be
+       understood by myself or other developers.
+      3. As a user, I want to have clear instructions, to avoid confusion.
+      4. As a developer, I want to commit my work often, to make troubleshooting
+       easier.
+
+Technologies used:
+html, css, javascript, ajax, jquery, gitHub, api, macOS, atom
+
+fixes in the future:
+
+styling is sickening
+code needs to be organized better and made more modular
+I need to create a multiplayer option
+make it possible to play stored games that havent been finished
+reduce redundancies
+
+Development Story:
+
+Absolute chaos.  My gameplan was nieve and lacking in detail.  I would have
+benefitedgreatly from detailed planning.  I worked on game logic for the
+majority of the time.  I got stuck and fixed on the problem instead of moving on
+to something I could do.The issue criteria was intimidating and I did not ask
+for help for the majority of the first two days.
+I strategized by writing out what I wanted to do, and trial-and-error. I used the
+console.log function and debugger as well.
--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -2,24 +2,13 @@
 
 const store = require('./store')
 const getFormFields = require('../../lib/get-form-fields')
-
-// const newGameTemplate =
-
 const api = require('./api')
 const ui = require('./ui')
-
-let turnNum = 0
-let gameOver = false
-// created empty arrays to hold the game-squares selected
-const xValues = []
-const oValues = []
-let playerTurn = 'X'
 // made a function to be invoked when clicking submit in the sign-up section
 const onSignUp = function (event) {
   event.preventDefault()
   const data = getFormFields(this)
-  console.log('this is this', this)
-  console.log('this is data', data)
+
   api.signUp(data)
     .then(ui.signInSuccess)
     .catch(ui.signInSuccess)
@@ -33,13 +22,12 @@ const addHandlers = () => {
   $('#new-game').on('submit', onNewGame)
   $('#game-index').on('submit', onGameIndex)
   // $('#find-game').on('submit', onFindGame)
-  $('#board').on('click', onUpdateGame)
+  $('.col-xs-4').on('click', onUpdateGame)
 }
-
+// function for signing in
 const onSignIn = function (event) {
   const data = getFormFields(this)
-  console.log('this is ', this)
-  console.log(data)
+
   event.preventDefault()
   api.signIn(data)
     .then(ui.signInSuccess)
@@ -49,46 +37,86 @@ const onSignIn = function (event) {
 const onChangePassword = function (event) {
   const data = getFormFields(this)
   event.preventDefault()
-  console.log('this is store:', store)
-  console.log(this)
+
   api.changePassword(data)
     .then(ui.changePasswordSuccess)
     .catch(ui.changePasswordFailure)
 }
 
-const onSignOut = function(event) {
+const onSignOut = function (event) {
   const data = getFormFields(this)
   event.preventDefault()
-  console.log(data)
-  console.log(this)
+
   api.signOut(data)
 }
 
+let turnNum = 0
+let gameOver = false
+// created empty arrays to hold the game-squares selected
+let xValues = []
+let oValues = []
+// declare player X as first player
+let playerTurn = 'X'
+// created a function that determines winner when the game ends
+const winCheck = function () {
+  if (xValues.length === 5 && gameOver === false) {
+    $('.message-box').text("It's a Draw")
+  } else if (gameOver === true && turnNum % 2 !== 0) {
+    $('.message-box').text('Player_X wins')
+  } else if (gameOver === true) {
+    $('.message-box').text('Player_O wins')
+  }
+}
 const onNewGame = function (event) {
   const data = getFormFields(this)
+  // reset all values to what it should be in a new game.
   $.each($('.box'), function (index, element) {
     $(element).text('')
   })
   turnNum = 0
+  gameOver = false
+  xValues = []
+  oValues = []
   event.preventDefault()
-  console.log('this is store:', store)
-  console.log(event)
-  console.log(this)
+
   api.newGame(data)
     .then(ui.newGameSuccess)
     .catch(ui.newGameFail)
 }
 
-const onGameIndex = function (event) {
-  const data = getFormFields(this)
-  event.preventDefault()
-  console.log(data)
-  console.log(this)
-  api.gameIndex()
-    .then(ui.gameIndexSuccess)
-    .catch(ui.gamefailure)
+// create function to check if winning combo is in the array
+const checkForMatch = function (valuesArray, tic, tac, toe) {
+  // match arguments with valuesArray to check for winner
+  if (valuesArray.indexOf(tic) !== -1 &&
+    valuesArray.indexOf(tac) !== -1 &&
+    valuesArray.indexOf(toe) !== -1) {
+    gameOver = true
+  }
 }
-
+// created funtion that controls turn taking
+const turnSwitch = () => {
+  const idOfPick = event.target.id
+  const textValue = $(event.target).text()
+  // prevent playing if game is over
+  if (gameOver === true) {
+    return
+    // prevent inputing a value on a square the has been used already
+  } else if (textValue === 'X' || textValue === 'O') {
+    // determine turn by checking if turn Number is even or odd
+  } else if (turnNum % 2 !== 0) {
+    // add to turnNum after execution
+    turnNum++
+    playerTurn = 'O'
+    $(event.target).text('O')
+    // push the id of the targeted event to respective arrays
+    oValues.push(idOfPick)
+  } else {
+    turnNum++
+    playerTurn = 'X'
+    $(event.target).text('X')
+    xValues.push(idOfPick)
+  }
+}
 // const onFindGame = function (event) {
 //   const data = getFormFields(event.target).game
 //   event.preventDefault()
@@ -100,68 +128,8 @@ const onGameIndex = function (event) {
 
 const onUpdateGame = function (event) {
   event.preventDefault()
-  const idOfPick = event.target.id
-  const textValue = $(event.target).text()
-  // create function to check if winning combo is in the array
-
-  const checkForMatch = function (valuesArray, tic, tac, toe) {
-    // make an array of arrays with winning combination of squares
-    const winningNumbers = [
-      [1, 2, 3],
-      [1, 5, 9],
-      [1, 4, 7],
-      [4, 5, 6],
-      [7, 8, 9],
-      [2, 5, 8],
-      [3, 6, 9],
-      [7, 5, 3]
-    ]
-    // loop through winningNumbers and assign the indexes of the arrays to
-    // arguments
-    // for (let i = 0; i < winningNumbers.length; i++) {
-    //  console.log('this is tic', winningNumbers[i][0])
-    //  console.log('this is tac', winningNumbers[i][1])
-    //  console.log('this is toe', winningNumbers[i][2])
-    //
-    // let tic = winningNumbers[i][0]
-    // let tac = winningNumbers[i][1]
-    // let toe = winningNumbers[i][2]
-    // match arguments with valuesArray to check for winner
-    if (valuesArray.indexOf(tic) !== -1 &&
-      valuesArray.indexOf(tac) !== -1 &&
-      valuesArray.indexOf(toe) !== -1) {
-      gameOver = true
-    } else if (xValues.length === 5 ) {
-      gameOver = true
-      console.log('Its a draw')
-    }
-  }
-
-  console.log('this is turnNum', turnNum)
-  // prevent inputing values if game has ended
-  if (gameOver === true) {
-    return
-    // prevent inputing a value on a square the has been used already
-  } else if (textValue === 'X' || textValue === 'O') {
-    console.log('pick a valid square')
-    // determine turn by checking if turn Number is even or odd
-  } else if (turnNum % 2 !== 0) {
-    // add to turnNum after execution
-    turnNum++
-    playerTurn = 'O'
-    $(event.target).text('O')
-    // push the id of the targeted event to respective arrays
-    oValues.push(event.target.id)
-    console.log('this is oValues', oValues)
-  } else {
-    turnNum++
-    playerTurn = 'X'
-    $(event.target).text('X')
-    xValues.push(idOfPick)
-    console.log('this is xValues', xValues)
-    console.log('this is event.target', event.target)
-  }
-
+  // switch turns and turn off when there is a winner
+  turnSwitch()
   // invoke the function to check of a winner
   checkForMatch(xValues, '1', '2', '3')
   checkForMatch(oValues, '1', '2', '3')
@@ -179,30 +147,32 @@ const onUpdateGame = function (event) {
   checkForMatch(oValues, '3', '6', '9')
   checkForMatch(xValues, '7', '5', '3')
   checkForMatch(oValues, '7', '5', '3')
-  endGame()
+  // if winner is found the game is ended and a draw or winner is declared
+  winCheck()
+  // update api as the game is played
   const data = {
     'game': {
       'cell': {
+        // api expects inputs ranging 0-8
+        // my id values are ranged 1-9
+        // added a - 1 to account for this
         'index': event.target.id - 1,
         'value': playerTurn
       },
       'over': gameOver
     }
   }
-  console.log('game is ', data)
+
   api.updateGame(data)
     .then(ui.updateGameSuccess)
     .catch(ui.failure)
 }
 
-const endGame = function () {
-  if (gameOver === true && turnNum !== 0) {
-    console.log('gameover')
-    console.log('player_x wins')
-  } else if (gameOver === true) {
-    console.log('gameover')
-    console.log('player_o wins')
-  }
+const onGameIndex = function (event) {
+  event.preventDefault()
+  api.gameIndex()
+    .then(ui.gameIndexSuccess)
+    .catch(ui.gamefailure)
 }
 
 module.exports = {
